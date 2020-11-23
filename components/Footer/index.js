c780f148b7b4ad624ae229d8640e2fcab0e1bb3e
Chanh Nguyen <chanhnguyen@chanhs-mbp.lan>
2020-11-22 20:21:39 -0700

    style footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,7 +1,73 @@
+import styles from './styles.module.scss'
+
 export default function Footer() {
   return (
-    <footer>
-      I am a footer!
+    <footer className={styles.container}>
+      <div className={styles.inner}>
+        <div>
+          <img src="/logo-white.svg" alt="" />
+          <p className={styles.tagline}>
+            Trafalgar provides progressive, and affordable healthcare,
+            accessible on mobile and online for everyone
+          </p>
+          <p className={styles.copyright}>
+            ©Trafalgar PTY LTD 2020. All rights reserved
+          </p>
+        </div>
+        <div className={styles.linksColumns}>
+          <div>
+            <p className={styles.linksHeading}>Company</p>
+            <ul className={styles.links}>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
+              <li>
+                <a href="#">Find a doctor</a>
+              </li>
+              <li>
+                <a href="#">Apps</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className={styles.linksHeading}>Region</p>
+            <ul className={styles.links}>
+              <li>
+                <a href="#">Indonesia</a>
+              </li>
+              <li>
+                <a href="#">Singapore</a>
+              </li>
+              <li>
+                <a href="#">Hongkong</a>
+              </li>
+              <li>
+                <a href="#">Canada</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className={styles.linksHeading}>Help</p>
+            <ul className={styles.links}>
+              <li>
+                <a href="#">Help center</a>
+              </li>
+              <li>
+                <a href="#">Contact support</a>
+              </li>
+              <li>
+                <a href="#">Instructions</a>
+              </li>
+              <li>
+                <a href="#">How it works</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
